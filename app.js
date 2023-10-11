3b42b5be7e70a22e774a54567d97479edfe3052d
salamanderctesiphon <swbrookshire@gmail.com>
2023-10-11 18:41:59 -0500

    working through the lesson

---
+++ app.js
@@ -0,0 +1,19 @@
+// Object literal syntax example
+
+const myObject = {
+  property: "Value!",
+  otherProperty: 77,
+  "obnoxious property": function() {
+    ///do stuff
+  }
+}
+
+// There two ways to get info out  of an object
+// Dot notation and bracket notation
+
+myObject.property
+
+myObject["obnoxious property"]
+
+
+
