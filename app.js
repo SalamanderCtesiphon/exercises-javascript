d4b82acb4565610ee25e0a5aa60aa9840c00a629
salamanderctesiphon <swbrookshire@gmail.com>
2023-10-11 18:45:45 -0500

    copied the example

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
 // Object literal syntax example
 
-const myObject = {
+/* const myObject = {
   property: "Value!",
   otherProperty: 77,
   "obnoxious property": function() {
     ///do stuff
   }
-}
+} */
 
 // There two ways to get info out  of an object
 // Dot notation and bracket notation
 
-myObject.property
+/* myObject.property
 
-myObject["obnoxious property"]
+myObject["obnoxious property"] */
 
+function Player(name, marker) {
+  this.name = name
+  this.marker = marker
+}
 
+const player = new Player('steve', 'X')
+console.log(player.name)
 
