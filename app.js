01d68309f2928d47de34f19b93c11be340ba00cb
salamanderctesiphon <swbrookshire@gmail.com>
2023-10-11 19:34:45 -0500

    a commit

--- app.js
+++ app.js
@@ -1,30 +1,3 @@
-// Object literal syntax example
-
-/* const myObject = {
-  property: "Value!",
-  otherProperty: 77,
-  "obnoxious property": function() {
-    ///do stuff
-  }
-} */
-
-// There two ways to get info out  of an object
-// Dot notation and bracket notation
-
-/* myObject.property
-
-myObject["obnoxious property"] */
-
-function Player(name, marker) {
-  this.name = name
-  this.marker = marker
-  this.sayName = function() {
-    console.log(name)
-  }
-}
-
-const player1 = new Player('steve', 'X')
-const player2 = new Player("also steve", "O")
 
 
 function Book(title, author, pages, haveRead) {
@@ -38,4 +11,3 @@ function Book(title, author, pages, haveRead) {
 
 const theHobbit = new Book('The Hobbit', 'JRR Tolkien', 295, 'have read')
 
-console.log(Object.getPrototypeOf(player1))
